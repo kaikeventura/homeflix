724081e916b8d0f405f3b83f4dd85104ec61bc4f
kaikeventura <kaike.gnr@hotmail.com>
2023-10-05 21:17:25 -0300

    first commit

---
+++ src/main/kotlin/com/kaikeventura/homeflix/HomeflixApplication.kt
@@ -0,0 +1,11 @@
+package com.kaikeventura.homeflix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomeflixApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomeflixApplication>(*args)
+}
---
+++ src/test/kotlin/com/kaikeventura/homeflix/HomeflixApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kaikeventura.homeflix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomeflixApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
